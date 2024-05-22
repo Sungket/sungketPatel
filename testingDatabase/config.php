de14c4871c3cce4df8137c11b521f7bc3a394894
sungketpatel <93491754+Sungket@users.noreply.github.com>
2024-05-22 07:08:01 +0100

    added arrays to output

---
+++ testingDatabase/config.php
@@ -0,0 +1,13 @@
+<?php
+    //connect to the local database
+
+    $cd_host = "localhost";
+    $cd_port = 8888;
+    $cd_socket = "";
+
+    //db name, username and password
+    $cd_dbname = "companydirectory";
+    $cd_user = "root";
+    $cd_password = "root";
+
+?>
---
+++ testingDatabase/grabAll.php
@@ -0,0 +1,36 @@
+<?php
+    
+    include("config.php");
+
+    $conn = new mysqli($cd_host, $cd_port, $cd_socket, $cd_dbname, $cd_user, $cd_password);
+
+    if ($conn -> connect_errno) {
+        echo "Failed to connect to database";
+        mysqli_close($conn);
+        exit();
+    }
+
+    $query = $conn->prepare('SELECT firstName FROM personnel');
+
+    $query->bind_param("s", $firstName);
+
+    $result = $conn->query($query);
+
+    if (!$result) {
+        $output['status']['code'] = '400';
+        mysqli_close($conn);
+        echo json_encode($output);
+
+        exit();
+    }
+
+    $data = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($data, $row);
+    }
+    $output['data'] = $data;
+
+    mysqli_close($conn);
+
+?>
